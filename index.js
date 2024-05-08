ddaca998d5e8f04dd146db228cebd4824d7e5d8d
Shubham Gupta <Shubham.Gupta@ideas.com>
2024-05-08 10:21:53 -0500

    Setup

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+
+const app = express();
+const port = 3000;
+let log = console.log;
+
+const db = new pg.Client({
+  host: "localhost",
+  user: "postgres",
+  password: "password",
+  database: "toDoList",
+  port: 5432
+});
+
+
+// Middleware
+db.connect();
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", async (req, res) => {
+
+
+  res.render('index.ejs');
+
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
